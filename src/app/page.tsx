8a41cb8d4d921785c62069d673b729f114bc58bd
saiful <saifulmuhammad414@gmail.com>
2023-04-05 23:30:29 +0700

    init development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
+       <p className="text-13xl underline">
+          Hello world!
+        </p>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
